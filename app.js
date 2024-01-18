7ce942afbda479b046c44212d998f4c7f5a25b53
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-18 17:10:17 -0300

    integração com o html

---
+++ app.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'número secreto';
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'escolha um número entre 1 a 10';
+
+function verificarChute() {
+    console.log('o botão foi clicado')
+}
