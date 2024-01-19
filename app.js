bd8ae31ac55e30b2bd0810cb7f92516b8680229e
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-19 13:03:47 -0300

    funções

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'número secreto';
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'escolha um número entre 1 a 10';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Número secreto';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'escolha um número entre 1 a 10';
 
+let numeroSecreto = gerarUmNumeroAleatorio()
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto 
+}
+
+exibirTextoNaTela('h1', 'Número secreto')
+exibirTextoNaTela('P', 'Escolha um número entre 1 a 10')
+  
 function verificarChute() {
-    console.log('o botão foi clicado')
+    console.log('o botão foi clicado');
+}
+function verificarChute() {
+    console.log(numeroSecreto);
+}
+function gerarUmNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1)
 }
